e890969ade887a1448d793facfdbb1d791dbb6a4
nownabe <nownabe@gmail.com>
2017-05-11 00:15:15 +0900

    :sparkles: Show memory

---
+++ src/components/Dashboard/Computer/Memory/index.jsx
@@ -0,0 +1,29 @@
+import React from "react";
+import { connect } from "react-redux";
+
+const c = ({ memory }) => (
+  <table id="memory" className="table is-narrow is-striped">
+    <thead>
+      <tr>
+        <th>Address</th>
+        <th>Value</th>
+        <th>Letter</th>
+      </tr>
+    </thead>
+    <tbody>
+      {
+        memory.map((v, i) => (
+          <tr key={`addr${i}`}>
+            <td>{i}</td>
+            <td>{v}</td>
+            <td>{String.fromCharCode(v)}</td>
+          </tr>
+        ))
+      }
+    </tbody>
+  </table>
+)
+
+export default connect(
+  ({ memory }) => ({ memory })
+)(c);
--- src/components/Dashboard/Computer/index.jsx
+++ src/components/Dashboard/Computer/index.jsx
@@ -3,11 +3,13 @@ import React from "react";
 import Output from "./Output";
 import Source from "./Source";
 import Variables from "./Variables";
+import Memory from "./Memory";
 
 export default () => (
   <div className="column">
     <Output />
     <Source />
     <Variables />
+    <Memory />
   </div>
 );
