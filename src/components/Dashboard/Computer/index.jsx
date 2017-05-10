64729f19b745480973375dd5e522ad255fbbdc7d
nownabe <nownabe@gmail.com>
2017-05-11 01:14:00 +0900

    :fire: Remove variables table

--- src/components/Dashboard/Computer/index.jsx
+++ src/components/Dashboard/Computer/index.jsx
@@ -2,14 +2,12 @@ import React from "react";
 
 import Output from "./Output";
 import Source from "./Source";
-import Variables from "./Variables";
 import Memory from "./Memory";
 
 export default () => (
   <div className="column">
     <Output />
     <Source />
-    <Variables />
     <Memory />
   </div>
 );
