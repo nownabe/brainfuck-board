0f08edd1cb6653abc526b46e530b89de1a014506
nownabe <nownabe@gmail.com>
2017-05-28 17:52:14 +0900

    :rocket: Setup firebase deployment

--- webpack.config.js
+++ webpack.config.js
@@ -1,18 +1,24 @@
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+
 module.exports = {
   context: __dirname + "/src",
   entry: "./index.ts",
   devtool: "source-map",
   output: {
-    path: __dirname,
+    path: __dirname + "/public",
     filename: "bundle.js"
   },
   module: {
     loaders: [
-      { test: /\.tsx?$/, exclude: /node_modules/, loader: "ts-loader" }
+      { test: /\.tsx?$/, exclude: /node_modules/, loader: "ts-loader" },
+      { test: /\.html$/, exclude: /node_modules/, loader: "html-loader" }
     ]
   },
   resolve: {
     modules: [__dirname + "/src", __dirname + "/node_modules"],
     extensions: [".ts", ".tsx", ".js"]
-  }
+  },
+  plugins: [
+    new HtmlWebpackPlugin({ template: "index.html" })
+  ]
 }
