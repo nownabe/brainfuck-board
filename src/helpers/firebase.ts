3d9a121680f03b26fb906586e69c6ae0004dec89
nownabe <nownabe@gmail.com>
2019-11-27 12:34:42 +0900

    Add firebase app id config

--- src/helpers/firebase.ts
+++ src/helpers/firebase.ts
@@ -12,6 +12,7 @@ firebase.initializeApp({
     messagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID,
     projectId: process.env.FIREBASE_PROJECT_ID,
     storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
+    appId: process.env.FIREBASE_APP_ID,
 });
 
 export const init = (dispatch: Dispatch<Action>) => {
