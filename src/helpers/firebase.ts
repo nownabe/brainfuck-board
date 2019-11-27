7813b33f646ff960323871e71519f3aa7d175d46
nownabe <nownabe@gmail.com>
2019-11-27 12:46:53 +0900

    :tshirt: Lint

--- src/helpers/firebase.ts
+++ src/helpers/firebase.ts
@@ -7,12 +7,12 @@ import { Program, Source, User } from "states";
 
 firebase.initializeApp({
     apiKey: process.env.FIREBASE_API_KEY,
+    appId: process.env.FIREBASE_APP_ID,
     authDomain: process.env.FIREBASE_AUTH_DOMAIN,
     databaseURL: process.env.FIREBASE_DATABASE_URL,
     messagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID,
     projectId: process.env.FIREBASE_PROJECT_ID,
     storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
-    appId: process.env.FIREBASE_APP_ID,
 });
 
 export const init = (dispatch: Dispatch<Action>) => {
