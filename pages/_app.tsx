a7836e741431f2378660629be98ee35829f0f6dd
nownabe <nownabe@users.noreply.github.com>
2022-02-26 16:50:49 +0900

    Add css for icons manually

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,7 @@
 import type { AppProps } from "next/app";
 import { RecoilRoot } from "recoil";
 
+import "../styles/global.css";
 import Layout from "../components/Layout";
 
 function MyApp({ Component, pageProps }: AppProps) {
